31f795ca94ae83a730f1e4ced88379efcd557313
Kyle Causton <kcauston@rossvideo.com>
2022-11-11 13:02:35 -0500

    Added directory

---
+++ src/components/category-list/category-list.component.jsx
@@ -0,0 +1,12 @@
+import './category-list.styles.scss'
+import CategoryItem from '../category-item/category-item.component';
+
+const CategoryList = ({categories}) => (
+    <div className="categories-container">
+        {categories.map((category) => (
+            <CategoryItem key={category.id} category={category}/>
+        ))}
+    </div>
+);
+
+export default CategoryList;
