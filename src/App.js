8c2b363aa5ba41944bbeb2ad4ae3de1081c1ddde
Kyle Causton <kcauston@rossvideo.com>
2022-11-11 15:10:24 -0500

    Nav bar!

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import CategoryList from './components/category-list/category-list.component';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
-
-  const categories = [
-      {
-        "id": 1,
-        "title": "Hats",
-        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        "id": 2,
-        "title": "Jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        "id": 3,
-        "title": "Sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        "id": 4,
-        "title": "Womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        "id": 5,
-        "title": "Mens",
-        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-      }
-  ];
-
   return (
     <div>
-      <CategoryList categories={categories}/>
+      <Routes>
+        <Route path="/" element={<Navigation/>}>
+          <Route index element={<Home/>}/>
+          <Route path="shop" element={<h2>a shop</h2>}/>
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
